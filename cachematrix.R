305ba058e285d3337bee7226f83aef05007c64d3
BenGalvin <benglvn@gmail.com>
2015-03-22 10:52:27 +0000

    Functions defined and tested

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,51 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(data_matrix = matrix()) {
+  
+    stored_inv <- NULL
+    
+    set <- function(y) {
+        data_matrix <<- y
+        stored_inv <<- NULL
+    }
+    
+    get <- function() {
+        return (data_matrix)
+    }
+    
+    setinv <- function(sent_replacement_inv){
+        stored_inv <<- sent_replacement_inv
+    }
+    
+    getinv <- function() {
+        return(stored_inv)
+    }
+    
+    
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
+    
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(madeMatrix, ...) {
         ## Return a matrix that is the inverse of 'x'
+    
+    local_inv <- madeMatrix$getinv()
+    
+    if(!is.null(local_inv)) {
+        message("getting cached data")
+        return(local_inv)
+    }
+    else {
+        local_data <- madeMatrix$get()
+        local_inv <- solve(local_data, ...)
+        
+        madeMatrix$setinv(local_inv)
+        return(local_inv)
+    }
 }
