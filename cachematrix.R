ab81cb772e8732527cbf86f353661f975dc6aef7
BenGalvin <benglvn@gmail.com>
2015-03-22 11:27:07 +0000

    Added comments to the code

--- cachematrix.R
+++ cachematrix.R
@@ -1,26 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The pair of functions below are used to cache the inverse of a matrix and return this cached value 
+## if it has already been calculated
 
-## Write a short comment describing this function
+
+
+## The makeCacheMatrix function creates a list containing a function to set the value of the matrix,
+## get the value of the matrix, set the inverse of the matrix and get the inverse of the matrix
 
 makeCacheMatrix <- function(data_matrix = matrix()) {
   
     stored_inv <- NULL
     
-    set <- function(y) {
+    set <- function(y) {                            ## Sets the value of the matrix
         data_matrix <<- y
         stored_inv <<- NULL
     }
     
-    get <- function() {
+    get <- function() {                             ## Returns the value of the matrix
         return (data_matrix)
     }
     
-    setinv <- function(sent_replacement_inv){
+    setinv <- function(sent_replacement_inv){       ## Sets the calculated inverse of the matrix
         stored_inv <<- sent_replacement_inv
     }
     
-    getinv <- function() {
+    getinv <- function() {                          ## Returns the cached inverse of the matrix
         return(stored_inv)
     }
     
@@ -32,22 +35,22 @@ makeCacheMatrix <- function(data_matrix = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve function computes the inverse of the special "matrix" returned by the makeCacheMatrix function.
+## If the inverse has already been calculated then the inverse from the cache is returned
 
-cacheSolve <- function(madeMatrix, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) {
     
-    local_inv <- madeMatrix$getinv()
+    local_inv <- x$getinv()                 ## Get the cached inverse of the matrix
     
-    if(!is.null(local_inv)) {
+    if(!is.null(local_inv)) {               ## Checks if inverse has been cached (is not null)
         message("getting cached data")
-        return(local_inv)
+        return(local_inv)                   ## returns cached result
     }
     else {
-        local_data <- madeMatrix$get()
-        local_inv <- solve(local_data, ...)
+        local_data <- x$get()
+        local_inv <- solve(local_data, ...) ## calculates the inverse of the matrix
         
-        madeMatrix$setinv(local_inv)
-        return(local_inv)
+        x$setinv(local_inv)                 ## caches the result
+        return(local_inv)                   ## returns the calculated inverse
     }
 }
